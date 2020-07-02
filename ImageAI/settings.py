4dfae8f981196adf3931615ce202624dc9e204ae
Gauransh Sawhney <45942718+garrysawyers3007@users.noreply.github.com>
2020-07-02 17:50:38 +0530

    Update settings.py

--- ImageAI/settings.py
+++ ImageAI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'acqo(!zchh0*1auhjm#20w6xhze@^xgejjz26wf()+4g#zi4wt'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
